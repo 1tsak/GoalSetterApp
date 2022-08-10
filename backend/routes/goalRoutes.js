2e9a52bd867d0f3e51abeca3dc90fd31ec473e35
A2v10 <hey.a2v10@gmail.com>
2022-08-10 20:02:19 +0530

    Added Basic CRUD Routes || GoalsRoutes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,6 +1,15 @@
 const express = require('express')
-const router = express.Router
+const router = express.Router()
 router.get('/',(req,res)=>{
-    res.status(200).json({message:'Get Goals route'})
+    res.status(200).json({message:'Get Goals'})
+})
+router.post('/',(req,res)=>{
+    res.status(200).json({message:'Set Goal'})
+})
+router.put('/:id',(req,res)=>{
+    res.status(200).json({message:`Update goal ${req.params.id}`})
+})
+router.delete('/:id',(req,res)=>{
+    res.status(200).json({message:`Delete goal ${req.params.id}`})
 })
 module.exports = router
