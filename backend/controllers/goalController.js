abbc5d02ea6d8421a4fb0e9914093f2947499331
A2v10 <hey.a2v10@gmail.com>
2022-08-11 09:43:57 +0530

    Added Middleware || Custom Express Error Handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -10,6 +10,10 @@ const getGoals =(req,res)=>{
 // @route  POST /api/goals
 // @access Private
 const setGoal =(req,res)=>{
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
     res.status(200).json({message:'Set Goal'})
 }
 
