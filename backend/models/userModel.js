7bcc8526d6e1d71ff30d9ead5d0757f2ffed1584
A2v10 <hey.a2v10@gmail.com>
2022-08-13 11:36:53 +0530

    Added User Model Schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,23 @@
+const { default: mongoose } = require("mongoose");
+const Mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
