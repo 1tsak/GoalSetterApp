b5f3b5cec57ca3996461a057a2526b735b24ff53
A2v10 <hey.a2v10@gmail.com>
2022-08-29 17:47:30 +0530

    fixed jwt bug

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -15,6 +15,9 @@ const protect = asyncHandler(async (req, res, next) => {
       //Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select('-password')
+
       next();
     } catch (error) {
       console.log(error);
